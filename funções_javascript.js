42b8b7e3ac4ae70161c173aba5e0cd9753725633
paulohenrique2007 <105729058+paulohenrique2007@users.noreply.github.com>
2024-04-17 13:59:42 -0300

    Create funções_javascript.js

---
+++ funções_javascript.js
@@ -0,0 +1,12 @@
+function somar (num1, num2{
+  return num1+num2;
+
+function diminuir (num1, num2)
+   return num1-num2;
+
+function multiplicar (num1, num2)
+  return num1*num2;
+
+funcition dividir (num1, num2)
+  return num1/num2;
+}
